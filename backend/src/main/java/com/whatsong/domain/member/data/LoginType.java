f20a3e09250ffdb10a63fb1911150e6be8556463
NoRuTnT <moonabcd@naver.com>
2025-07-28 14:24:58 +0900

    Feat: 소셜,일반 로그인타입 추가, db converter추가

---
+++ backend/src/main/java/com/whatsong/domain/member/data/LoginType.java
@@ -0,0 +1,28 @@
+package com.whatsong.domain.member.data;
+
+import java.util.Arrays;
+
+import com.whatsong.global.exception.exception.MemberException;
+import com.whatsong.global.exception.type.MemberExceptionType;
+
+import lombok.Getter;
+
+@Getter
+public enum LoginType {
+	SOCIAL("SOCIAL"),
+	SIMPLE("SIMPLE");
+
+	private final String type;
+
+	LoginType(String type) {
+		this.type = type;
+	}
+
+	public static LoginType ofType(String type) {
+		return Arrays.stream(LoginType.values())
+			.filter(value -> value.getType().equals(type))
+			.findAny()
+			.orElseThrow(() -> new MemberException(MemberExceptionType.INVALID_LOGIN_TYPE));
+	}
+
+}
---
+++ backend/src/main/java/com/whatsong/domain/member/data/LoginTypeConverter.java
@@ -0,0 +1,17 @@
+package com.whatsong.domain.member.data;
+
+import jakarta.persistence.AttributeConverter;
+
+public class LoginTypeConverter implements AttributeConverter<LoginType, String> {
+
+	@Override
+	public String convertToDatabaseColumn(LoginType attribute) {
+		return attribute.getType();
+	}
+
+	@Override
+	public LoginType convertToEntityAttribute(String dbData) {
+		return LoginType.ofType(dbData);
+	}
+
+}
