3a943b11a73c8a5e98f5a9e163830a4464168fc7
NoRuTnT <moonabcd@naver.com>
2025-07-28 14:17:31 +0900

    Feat: api응답 표준화를위한 BaseResponse추가

---
+++ backend/src/main/java/com/whatsong/global/common/response/BaseResponse.java
@@ -0,0 +1,20 @@
+package com.whatsong.global.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseResponse<T> {
+	@Builder.Default
+	private Integer code = 200;
+
+	@Builder.Default
+	private String message = "SUCCESS";
+
+	private T data;
+}
