68b6a889641b7d372318f0d2c43a699c016fb07e
NoRuTnT <moonabcd@naver.com>
2025-07-31 05:23:09 +0900

    refactor: redisconfig 리팩토링

--- backend/src/main/java/com/whatsong/global/config/RedisConfig.java
+++ backend/src/main/java/com/whatsong/global/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 public class RedisConfig {
@@ -20,20 +21,28 @@ public class RedisConfig {
 
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
-		redisConfiguration.setHostName(host);
-		redisConfiguration.setPort(port);
-		redisConfiguration.setPassword(password);
-		LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisConfiguration);
-		return lettuceConnectionFactory;
+		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+		config.setPassword(password);
+		return new LettuceConnectionFactory(config);
 	}
 
 	@Bean
-	public RedisTemplate<String, String> redisTemplate() {
-		RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-		redisTemplate.setKeySerializer(new StringRedisSerializer()); // key 깨짐 방지
-		redisTemplate.setValueSerializer(new StringRedisSerializer()); // value 깨짐 방지
-		redisTemplate.setConnectionFactory(redisConnectionFactory());
-		return redisTemplate;
+	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, String> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+
+		// String 직렬화 설정
+		RedisSerializer<String> stringSerializer = new StringRedisSerializer();
+
+		// Key 직렬화
+		template.setKeySerializer(stringSerializer);
+		template.setHashKeySerializer(stringSerializer);
+
+		// Value 직렬화
+		template.setValueSerializer(stringSerializer);
+		template.setHashValueSerializer(stringSerializer);
+
+		template.afterPropertiesSet();
+		return template;
 	}
 }
