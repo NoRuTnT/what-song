e4361459bf7cea184919923ea2bb2bf6c2b12c7e
NoRuTnT <moonabcd@naver.com>
2025-10-29 08:20:22 +0900

    fix: 파일경로이름 Service->service 수정

--- backend/src/main/java/com/whatsong/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/whatsong/domain/member/controller/MemberController.java
@@ -3,16 +3,12 @@ package com.whatsong.domain.member.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.whatsong.domain.auth.dto.responseDto.ValidateDuplicatedLoginIdResponseDto;
-import com.whatsong.domain.member.Service.MemberInfoService;
-import com.whatsong.domain.member.Service.MemberInfoServiceImpl;
+import com.whatsong.domain.member.service.MemberInfoService;
 import com.whatsong.domain.member.dto.responseDto.MemberInfoResponseDto;
-import com.whatsong.domain.member.repository.MemberInfoRepository;
 import com.whatsong.global.common.response.BaseResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
